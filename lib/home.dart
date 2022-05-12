b4a5779e5f3cd4c6e097bd052fb5dacbcade1cd8
SInadumi <inazumi19@icloud.com>
2022-05-13 00:35:22 +0900

    Add PostPage in homepage

--- lib/home.dart
+++ lib/home.dart
@@ -74,6 +74,8 @@ class PostList extends StatelessWidget {
         mainAxisSize: MainAxisSize.max,
         children: [
           PostHeader(),
+          PostForm(),
+          PostButton(),
         ],
       ),
     );
@@ -88,6 +90,59 @@ class PostHeader extends StatelessWidget {
         Text('Icon'),
         Text('Read1er'),
       ],
+    );
+  }
+}
+
+class PostForm extends StatefulWidget {
+  @override
+  _PostFormState createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+  var ProfileContainer = [
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Expanded(
+        child: PageView.builder(
+          itemBuilder: (context, index) {
+            return ProfileContainer[index];
+          },
+          itemCount: ProfileContainer.length,
+        ),
+      ),
+    );
+  }
+}
+
+class PostButton extends StatefulWidget {
+  @override
+  _PostButton createState() => _PostButton();
+}
+
+class _PostButton extends State<PostButton> {
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 24),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.black,
+          padding: EdgeInsets.symmetric(horizontal: 48),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        ),
+        onPressed: () {},
+        child: Text('Done!', style: TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
